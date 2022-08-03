b37c401e2ceac39f1030acc60a1cafa88c16ec49
张森云 <zhangsy@citydo.com.cn>
2022-08-04 00:27:52 +0800

    大树形图

--- src/pages/Home/Center/components/Atlas/components/AtlasMap/ContrastSvgBox/index.vue
+++ src/pages/Home/Center/components/Atlas/components/AtlasMap/ContrastSvgBox/index.vue
@@ -14,8 +14,8 @@ const props = defineProps({
     type: Number,
   },
 });
-let innerWidth, innerHeight, root;
-const g = ref('');
+let innerWidth, innerHeight, svg;
+// const g = ref('');
 const svgClass = ref('');
 onMounted(() => {
   svgClass.value = `svg-${props.index}`;
@@ -25,8 +25,7 @@ watch(
   (val) => {
     console.log('变啦');
     console.log(val);
-    if (val.children && val.children.length > 0) {
-      console.log('可以画啦');
+    if (val.children) {
       nextTick(() => {
         init();
       });
@@ -35,38 +34,76 @@ watch(
   },
   { immediate: true, deep: true },
 );
+// 节点背景颜色
+function rectColor(d) {
+  return d.data.same ? '#3B78F2' : '#142847';
+}
+const nodeOption = {
+  height: (name) => {
+    // 动态设置文本框宽度
+    return name.length * 18 + 15;
+  },
+  width: 32,
+};
+const nodeOption2 = {
+  width: (name) => {
+    // 动态设置文本框宽度
+    return name.length * 18 + 15;
+  },
+  height: 32,
+};
+const margin = {
+  top: 40,
+  right: 30,
+  bottom: 33,
+  left: 30,
+};
+const midValue = 322;
 function init() {
-  console.log('画啦');
-  const svg = d3.select(`.svg-${props.index}`);
+  svg = d3.select(`.svg-${props.index}`);
   if (svg.select('g')) {
-    svg.select('g').remove();
+    svg.selectAll('g').remove();
   }
-  const margin = {
-    top: 40,
-    right: 30,
-    bottom: 33,
-    left: 30,
-  };
-  const nodeOption = {
-    width: (name) => {
-      // 动态设置文本框宽度
-      return name.length * 18 + 15;
-    },
-    height: 32,
-  };
   const width = parseInt(svg.style('width'), 10);
   const height = parseInt(svg.style('height'), 10);
   innerWidth = width - margin.left - margin.right;
   innerHeight = height - margin.top - margin.bottom;
-  g.value = svg.append('g').attr('transform', `translate(${margin.top},${margin.left})`);
-  root = d3.hierarchy(props.data);
-  root = d3.tree().size([innerWidth, innerHeight])(root);
+  if (props.data.children[0]) {
+    render(props.data.children[0], 'left');
+  }
+  if (props.data.children[1]) {
+    render(props.data.children[1], 'right');
+    let data = {
+      name: props.data.name,
+      children: [
+        {
+          name: props.data.children[0].name,
+        },
+        {
+          name: '相同节点',
+        },
+        {
+          name: props.data.children[1].name,
+        },
+      ],
+    };
+    let sameData = {
+      name: '相同节点',
+      children: props.data.children[1].children,
+    };
+    renderSame(sameData);
+    renderAll(data);
+  }
+}
+function render(data, position) {
+  const g = svg.append('g').attr('transform', `translate(${margin.top},${margin.left})`);
+  let root = d3.hierarchy(data);
+  root = d3.tree().size([innerHeight / 2, innerWidth / 3])(root);
   const node = root.descendants();
   const path = root.links();
   console.log(node, path);
   // 画线
-  g.value
-    .selectAll('path')
+  g.selectAll('path')
     .data(path)
     .join('path')
     .attr('fill', 'none')
@@ -75,32 +112,29 @@ function init() {
     .attr(
       'd',
       d3
-        .linkHorizontal()
-        .x((d) => d.x)
-        .y((d) => d.y),
+        .linkVertical()
+        .x((d) => (position === 'right' ? innerWidth / 2 + d.y + midValue / 2 : innerWidth / 2 - d.y - midValue / 2))
+        .y((d) => d.x),
     );
-  // 节点背景颜色
-  function rectColor(d) {
-    console.log(d);
-    return d.data.same ? '#3B78F2' : '#142847';
-  }
   // 画节点方块
-  g.value
-    .selectAll('rect')
+  g.selectAll('rect')
     .data(node)
     .join('rect')
-    .attr('width', (d) => nodeOption.width(d.data.name))
-    .attr('height', nodeOption.height)
+    .attr('width', (d) => nodeOption.height(d.data.name))
+    .attr('height', nodeOption.width)
     .attr('fill', (d) => rectColor(d))
     .attr('stroke', '#3B78F2')
     .attr('stroke-width', 2)
-    .attr('x', (d) => d.x - nodeOption.width(d.data.name) / 2)
-    .attr('y', (d) => d.y)
+    .attr('x', (d) =>
+      position === 'right'
+        ? innerWidth / 2 + d.y - nodeOption.height(d.data.name) / 2 + midValue / 2
+        : innerWidth / 2 - (d.y + nodeOption.height(d.data.name) / 2) - midValue / 2,
+    )
+    .attr('y', (d) => d.x - nodeOption.width / 2)
+    .attr('opacity', (d) => (d.depth === 0 && props.data.children.length === 2 ? 0 : 1))
     .attr('class', 'rect');
-
   // 文字
-  g.value
-    .selectAll('text')
+  g.selectAll('text')
     .data(node)
     .join('text')
     .on('click', (d) => {
@@ -111,13 +145,127 @@ function init() {
       }
     })
     .attr('text-anchor', 'middle') // 位置
-    .attr('x', (d) => d.x)
-    .attr('y', (d) => d.y + 23)
+    .attr('x', (d) =>
+      position === 'right' ? innerWidth / 2 + d.y + midValue / 2 : innerWidth / 2 - d.y - midValue / 2,
+    )
+    .attr('y', (d) => d.x + 6)
+    .text((d) => d.data.name)
+    .style('font-size', '18px')
+    .attr('opacity', (d) => (d.depth === 0 && props.data.children.length === 2 ? 0 : 1))
+    .attr('fill', 'white');
+  // .attr('writing-mode', 'vertical-rl') // 文本竖过来
+  // .attr('text-orientation', 'upright');
+  console.log(g);
+}
+function renderAll(data) {
+  const g = svg.append('g').attr('transform', `translate(${margin.top},${margin.left})`);
+  let root = d3.hierarchy(data);
+  root = d3.tree().size([innerWidth / 4, innerHeight / 4])(root);
+  const node = root.descendants();
+  const path = root.links();
+  console.log(node, path);
+  // 画线
+  g.selectAll('path')
+    .data(path)
+    .join('path')
+    .attr('fill', 'none')
+    .attr('stroke', '#7CA3EF')
+    .attr('stroke-width', 1)
+    .attr(
+      'd',
+      d3
+        .linkVertical()
+        .x((d) => d.x + (innerWidth * 3) / 8)
+        .y((d) => d.y),
+    );
+  // 画节点方块
+  g.selectAll('rect')
+    .data(node)
+    .join('rect')
+    .attr('width', (d) => nodeOption.height(d.data.name))
+    .attr('height', nodeOption.width)
+    .attr('fill', (d) => rectColor(d))
+    .attr('stroke', '#3B78F2')
+    .attr('stroke-width', 2)
+    .attr('x', (d) => d.x - nodeOption2.width(d.data.name) / 2 + (innerWidth * 3) / 8)
+    .attr('y', (d) => d.y - nodeOption2.height / 2)
+    .attr('class', 'rect');
+  // 文字
+  g.selectAll('text')
+    .data(node)
+    .join('text')
+    .on('click', (d) => {
+      console.log(d.target.__data__); // 点击的节点
+      console.log('点击了,深度为：' + d.target.__data__.depth);
+      if (d.target.__data__.depth) {
+        console.log('需要展示啦！');
+      }
+    })
+    .attr('text-anchor', 'middle') // 位置
+    .attr('x', (d) => d.x + (innerWidth * 3) / 8)
+    .attr('y', (d) => d.y + 6)
     .text((d) => d.data.name)
     .style('font-size', '18px')
     .attr('fill', 'white');
   // .attr('writing-mode', 'vertical-rl') // 文本竖过来
   // .attr('text-orientation', 'upright');
+  console.log(g);
+}
+function renderSame(data) {
+  const g = svg.append('g').attr('transform', `translate(${margin.top},${margin.left})`);
+  let root = d3.hierarchy(data);
+  root = d3.tree().size([innerWidth / 3, (innerHeight * 3) / 4])(root);
+  const node = root.descendants();
+  const path = root.links();
+  console.log(node, path);
+  // 画线
+  g.selectAll('path')
+    .data(path)
+    .join('path')
+    .attr('fill', 'none')
+    .attr('stroke', '#7CA3EF')
+    .attr('stroke-width', 1)
+    .attr(
+      'd',
+      d3
+        .linkVertical()
+        .x((d) => d.x + innerWidth / 3)
+        .y((d) => d.y + innerHeight / 4),
+    );
+  // 画节点方块
+  g.selectAll('rect')
+    .data(node)
+    .join('rect')
+    .attr('width', (d) => nodeOption.height(d.data.name))
+    .attr('height', nodeOption.width)
+    .attr('fill', (d) => rectColor(d))
+    .attr('stroke', '#3B78F2')
+    .attr('stroke-width', 2)
+    .attr('x', (d) => d.x - nodeOption2.width(d.data.name) / 2 + innerWidth / 3)
+    .attr('y', (d) => d.y - nodeOption2.height / 2 + innerHeight / 4)
+    .attr('class', 'rect')
+    .attr('opacity', (d) => (d.depth === 0 ? 0 : 1));
+  // 文字
+  g.selectAll('text')
+    .data(node)
+    .join('text')
+    .on('click', (d) => {
+      console.log(d.target.__data__); // 点击的节点
+      console.log('点击了,深度为：' + d.target.__data__.depth);
+      if (d.target.__data__.depth) {
+        console.log('需要展示啦！');
+      }
+    })
+    .attr('text-anchor', 'middle') // 位置
+    .attr('x', (d) => d.x + innerWidth / 3)
+    .attr('y', (d) => d.y + 6 + innerHeight / 4)
+    .text((d) => d.data.name)
+    .style('font-size', '18px')
+    .attr('opacity', (d) => (d.depth === 0 ? 0 : 1))
+    .attr('fill', 'white');
+  // .attr('writing-mode', 'vertical-rl') // 文本竖过来
+  // .attr('text-orientation', 'upright');
+  console.log(g);
 }
 </script>
 
